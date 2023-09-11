3ce496c34ba9d5917a7cc9931c3c385e0608e104
Leekukju <ckzmfk19@kakao.com>
2023-09-11 17:55:53 +0900

    todo-app END

--- src/todo/TodoApp.jsx
+++ src/todo/TodoApp.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ErrorComponent from "./ErrorComponent";
 import HeaderComponent from "./HeaderComponent";
 import ListTodosComponent from "./ListTodosComponent";
@@ -6,7 +6,17 @@ import LoginComponent from "./LoginComponent";
 import LogoutComponent from "./LogoutComponent";
 import "./TodoApp.css";
 import WelcomeComponent from "./WelcomeComponent";
-import AuthProvider from "./security/AuthContext";
+import AuthProvider, { useAuth } from "./security/AuthContext";
+
+function AuthenticatedRoute({ children }) {
+  const authContext = useAuth();
+  
+  if (authContext.isAuthenticated)
+    return (
+      children
+    );
+  return <Navigate to={"/"} />
+}
 
 export default function TodoApp() {
   return (
@@ -17,9 +27,21 @@ export default function TodoApp() {
           <Routes>
             <Route path="/" element={<LoginComponent />} />
             <Route path="/login" element={<LoginComponent />} />
-            <Route path="/welcome/:username" element={<WelcomeComponent />} />
-            <Route path="/todos" element={<ListTodosComponent />} />
-            <Route path="/logout" element={<LogoutComponent />} />
+            <Route path="/welcome/:username" element={
+              <AuthenticatedRoute>
+                <WelcomeComponent />
+              </AuthenticatedRoute>
+            } />
+            <Route path="/todos" element={
+              <AuthenticatedRoute>
+                <ListTodosComponent />
+              </AuthenticatedRoute>
+            } />
+            <Route path="/logout" element={
+              <AuthenticatedRoute>
+                <LogoutComponent />
+              </AuthenticatedRoute>
+            } />
 
             <Route path="*" element={<ErrorComponent />} />
           </Routes>
